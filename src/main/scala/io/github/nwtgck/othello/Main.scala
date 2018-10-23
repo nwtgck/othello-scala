8f80c8d9abe9d4f37c6f0eb8d5091aa5bf39fa86
Ryo Ota <nwtgck@gmail.com>
2018-10-24 00:28:00 +0900

    [change] Generalize input prompt and improve it

--- src/main/scala/io/github/nwtgck/othello/Main.scala
+++ src/main/scala/io/github/nwtgck/othello/Main.scala
@@ -7,69 +7,54 @@ import scala.util.Try
 
 object Main {
 
-  private def selectDepthLimit(): Int =
-    Iterator.continually(
-      Try(StdIn.readLine("depth limit> ").toInt).toOption
-    ).find { seedOpt =>
-      seedOpt.isDefined
-    }.head.get // NOTE: Logically safe .head.get because not empty sequence and defined
-
-  private def selectBoardEvaluator(): (Board, Disk) => Int = {
-    // Candidate players
-    val evaluators = Seq(
-      BoardEvaluators.uguisi _
-    )
-
-    val idx = Iterator.continually(
-      Try(StdIn.readLine("0: uguisu evaluator> ").toInt).toOption
-    ).find { idxOpt =>
-      idxOpt.isDefined && evaluators.isDefinedAt(idxOpt.get)
-    }.head.get // NOTE: Logically safe .head.get because not empty sequence and defined
-
-    evaluators(idx)
-  }
-
-  private def selectRandomSeed(): Long =
-    Iterator.continually(
-      Try(StdIn.readLine("seed> ").toLong).toOption
-    ).find { seedOpt =>
-      seedOpt.isDefined
-    }.head.get // NOTE: Logically safe .head.get because not empty sequence and defined
-
-  private def selectNTrials(): Int =
-    Iterator.continually(
-      Try(StdIn.readLine("# of trials> ").toInt).toOption
-    ).find { seedOpt =>
-      seedOpt.isDefined
-    }.head.get // NOTE: Logically safe .head.get because not empty sequence and defined
-
-  private def selectPlayer[D <: Disk](disk: D): Player[D] = {
-    // Candidate players
-    val delayedPlayers: Seq[() => Player[D]] =
-      Seq(
-        () => HumanPlayer(disk),
-        () => RandomPlayer(disk, selectRandomSeed()),
-        () => MonteCarloPlayer(disk, selectRandomSeed(), selectNTrials()),
-        () => UguisuEvaluationTablePlayer(disk),
-        () => MinimaxPlayer(disk, selectDepthLimit(), selectBoardEvaluator())
-      )
-
-    val playerIdx: Int = Iterator.continually(
-      Try(StdIn.readLine("0: human, 1: random, 2: Monte Carlo, 3: Uguisu table, 4: minimax> ").toInt).toOption
-    ).find { idxOpt =>
-      idxOpt.isDefined && delayedPlayers.isDefinedAt(idxOpt.get)
-    }.head.get // NOTE: Logically safe .head.get because not empty sequence and defined
-
-    delayedPlayers(playerIdx)()
-  }
-
   def main(args: Array[String]): Unit = {
-
     // Create players
-    var player1 = selectPlayer(Black)
-    var player2 = selectPlayer(White)
+    var player1 = selectDelayedPlayer("Player1", Black)()
+    var player2 = selectDelayedPlayer("Player2", White)()
 
     // Start the game
     new Game(player1, player2).start()
   }
+
+  private def selectValue[T](prefix: String, converter: String => T): T = {
+    Iterator.continually(
+      Try(converter(StdIn.readLine(s"${prefix}> "))).toOption
+    ).find { value =>
+      value.isDefined
+    }.head.get // NOTE: Logically safe .head.get because not empty sequence and defined
+  }
+
+  private def selectList[T](prefix: String, itemAndCandidates: Seq[(String, T)]): T = {
+    val items: Seq[String] = itemAndCandidates.map(_._1)
+    val num: Int = Iterator.continually(
+      Try(StdIn.readLine(s"${prefix}| ${items.zipWithIndex.map{case (item, i) => s"${i+1}: ${item}"}.mkString(" ")}> ").toInt).toOption
+    ).find { numOpt =>
+      numOpt.isDefined && itemAndCandidates.isDefinedAt(numOpt.get-1)
+    }.head.get // NOTE: Logically safe .head.get because not empty sequence and defined
+
+    itemAndCandidates(num -1)._2
+  }
+
+  private def selectDepthLimit(prefix: String): Int =
+    selectValue[Int](s"${prefix}/depth limit", _.toInt)
+
+  private def selectBoardEvaluator(prefix: String): (Board, Disk) => Int =
+    selectList(s"${prefix}/board evaluators", Seq(
+      ("uguisu", BoardEvaluators.uguisi)
+    ))
+
+  private def selectRandomSeed(prefix: String): Long =
+    selectValue(s"${prefix}/seed", _.toLong)
+
+  private def selectNTrials(prefix: String): Int =
+    selectValue(s"${prefix}/# of trials", _.toInt)
+
+  private def selectDelayedPlayer[D <: Disk](prefix: String, disk: D): () => Player[D] =
+    selectList(s"${prefix}", Seq(
+      ("human", () => HumanPlayer(disk)),
+      ("random", () => RandomPlayer(disk, selectRandomSeed(s"${prefix}/random"))),
+      ("Monte Carlo", () => MonteCarloPlayer(disk, selectRandomSeed(s"${prefix}/Monte Carlo"), selectNTrials(s"${prefix}/Monte Carlo"))),
+      ("Uguisu table", () => UguisuEvaluationTablePlayer(disk)),
+      ("minimax", () => MinimaxPlayer(disk, selectDepthLimit(s"${prefix}/minimax"), selectBoardEvaluator(s"${prefix}/minimax")))
+    ))
 }
